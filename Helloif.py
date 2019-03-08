1dfa62e20b701802d2d9e2416414d1db82600382
zonameng <zonameng@163.com>
2019-03-08 16:30:14 +0800

    22

---
+++ Helloif.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+spam=int(input('请输入数字： '))
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings')
+print(spam)
---
+++ change.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+tocal=0
+for i in range(101):
+    tocal = tocal + i 
+print(tocal)
