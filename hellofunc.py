5ecc3978e11b9072bfb34a8135d175c586f9d4b1
zonameng <zonameng@163.com>
2019-03-11 09:13:12 +0800

    this is of  def

---
+++ hellofunc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def hello():
+    print('hello')
+    print('howdy!!')
+hello()
+hello()
+hello()
+
+def Hello(name):
+    print('Hello  ' + name)
+    print('Howdy!!  ' + name)
+Hello('tom')
+Hello('alice')
