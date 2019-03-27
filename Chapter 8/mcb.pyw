8c5646c69975533aacc1384a81ab4713e5d5431a
zonameng <zonameng@163.com>
2019-03-27 17:28:10 +0800

    Chapter 8

---
+++ Chapter 8/mcb.pyw
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+# mcb.pyw - Saves and loads pieces of text to the clipboard.
---
+++ Chapter 8/textos.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import os 
+print(os.getcwd())
+osgetsize = os.path.getsize('E:\\ios\\cn_office_professional_plus_2016_x86_x64_dvd_6969182.iso')
+print(osgetsize)
+print(os.listdir('E:\\ios'))
